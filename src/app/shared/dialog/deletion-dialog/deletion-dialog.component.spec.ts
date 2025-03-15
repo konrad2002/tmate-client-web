c020a8c1ca7445b8be65985dc0d68d7dd111d74f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-03-15 20:26:29 +0100

    fix dialog tests

--- src/app/shared/dialog/deletion-dialog/deletion-dialog.component.spec.ts
+++ src/app/shared/dialog/deletion-dialog/deletion-dialog.component.spec.ts
@@ -1,23 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DeletionDialogComponent } from './deletion-dialog.component';
+import {DeletionDialogComponent} from './deletion-dialog.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('DeletionDialogComponent', () => {
-  let component: DeletionDialogComponent;
-  let fixture: ComponentFixture<DeletionDialogComponent>;
+    let component: DeletionDialogComponent;
+    let fixture: ComponentFixture<DeletionDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DeletionDialogComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [DeletionDialogComponent],
+            providers: [
+                {provide: MAT_DIALOG_DATA, useValue: {}},
+                {provide: MatDialogRef, useValue: {}},
+                provideHttpClient(),
+                provideHttpClientTesting(),
+            ]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(DeletionDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DeletionDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/shared/dialog/user/user-password-dialog/user-password-dialog.component.spec.ts
+++ src/app/shared/dialog/user/user-password-dialog/user-password-dialog.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserPasswordDialogComponent } from './user-password-dialog.component';
+import {UserPasswordDialogComponent} from './user-password-dialog.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 describe('UserPasswordDialogComponent', () => {
-  let component: UserPasswordDialogComponent;
-  let fixture: ComponentFixture<UserPasswordDialogComponent>;
+    let component: UserPasswordDialogComponent;
+    let fixture: ComponentFixture<UserPasswordDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UserPasswordDialogComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [UserPasswordDialogComponent],
+            providers: [
+                {provide: MAT_DIALOG_DATA, useValue: {}},
+                {provide: MatDialogRef, useValue: {}},
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                provideAnimations()
+            ]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(UserPasswordDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(UserPasswordDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
