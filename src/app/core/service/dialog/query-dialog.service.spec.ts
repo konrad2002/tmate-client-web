832e429e5d93be5a3526638f29377cb55e7e8904
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-03-15 21:38:14 +0100

    fix query dialog service test

--- src/app/core/service/dialog/query-dialog.service.spec.ts
+++ src/app/core/service/dialog/query-dialog.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QueryDialogService } from './query-dialog.service';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {provideHttpClient} from '@angular/common/http';
 
 describe('QueryDialogService', () => {
   let service: QueryDialogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+        providers: [
+            provideHttpClient(),
+            provideHttpClientTesting(),
+        ]
+    });
     service = TestBed.inject(QueryDialogService);
   });
 
